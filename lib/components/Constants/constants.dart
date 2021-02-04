afd93805023b1f64f61f721ad67c2c59eec530d8
Lokesh002 <lokeshj00@gmail.com>
2021-02-04 22:16:41 +0530

    Eleventh commit

---
+++ lib/components/Constants/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const kprimaryColor = Color(0xFF0A0E21);
+const ktoolbarColor = Color(0xFF0A0E21);
+const kinactiveColor = Color(0xFF2E7ABD);
+const kactiveColor = Colors.blue;
+const klabelColor = Color(0xFFB7BAC4);
+const ktextColor = Colors.white;
+const kbuttonColor = Color(0xFF212747);
+const ksliderThumbColor = Color(0xFFFE0167);
+const kresultTextColor = Color(0xFF55C893);
+const kNumberTextStyle = TextStyle(
+  fontWeight: FontWeight.w900,
+  color: Colors.white,
+  fontSize: 50,
+);
+const kLabelTextStyle = TextStyle(
+  color: Colors.white,
+);
+
+const String ipAddress =
+    'https://us-central1-sarvogyan-course-platform.cloudfunctions.net/';
+const String version = '1.0.0';
---
+++ lib/components/Networking/networking.dart
@@ -0,0 +1,67 @@
+import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:convert' as convert;
+
+import 'package:sarvogyan/components/Constants/constants.dart';
+
+class Networking {
+  String ip = ipAddress;
+
+  Future getData(String url) async {
+    String fullURL = ip + url;
+
+    http.Response getResponse = await http.get(
+      fullURL,
+      headers: {"Content-Type": "application/json"},
+    );
+
+    if (getResponse.statusCode == 200) {
+      String data = getResponse.body;
+      var decodedData = convert.jsonDecode(data);
+      return decodedData;
+    } else {
+      print(getResponse.statusCode);
+    }
+    return null;
+  }
+
+  Future postData(String url, var body) async {
+    String fullURL = ip + url;
+    http.Response postResponse = await http.post(fullURL,
+        headers: {"Content-Type": "application/json"},
+        body: convert.jsonEncode(body));
+
+    if (postResponse.statusCode == 200) {
+      String data = postResponse.body;
+
+      var decodedData = convert.jsonDecode(data);
+      return decodedData;
+    } else if (postResponse.statusCode == 404) {
+      print(postResponse.statusCode);
+      return postResponse.body;
+    } else {
+      print(postResponse.statusCode);
+    }
+
+    return null;
+  }
+
+  Future deleteData(String url) async {
+    String fullURL = ip + url;
+    http.Response postResponse = await http
+        .delete(fullURL, headers: {"Content-Type": "application/json"});
+
+    if (postResponse.statusCode == 200) {
+      String data = postResponse.body;
+
+      return data;
+    } else if (postResponse.statusCode == 404) {
+      print(postResponse.statusCode);
+      return postResponse.body;
+    } else {
+      print(postResponse.statusCode);
+    }
+
+    return null;
+  }
+}
